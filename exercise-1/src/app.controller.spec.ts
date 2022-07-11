fc21c5c4d3ddfe12557f2c55facc2c8024e44b23
Andres Tapia <wilson.tapia@rappi.com>
2022-07-10 20:10:32 -0500

    Add exercise 1

--- exercise-1/src/app.controller.spec.ts
+++ exercise-1/src/app.controller.spec.ts
@@ -15,8 +15,24 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return repositories data', () => {
+      const expectedData = {
+        repositories: [
+          {
+            id: 1,
+            state: 604,
+          },
+          {
+            id: 2,
+            state: 605,
+          },
+          {
+            id: 3,
+            state: 606,
+          },
+        ],
+      };
+      expect(appController.getRepositories()).toBe(expectedData);
     });
   });
 });
